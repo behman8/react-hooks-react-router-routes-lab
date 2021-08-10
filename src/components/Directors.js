b01889c2407088d559e9aa8fdcfbda85c90913ba
Branden <brandenehman45@gmail.com>
2021-08-09 19:01:25 -0600

    Completed Lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { directors } from "../data";
+import { v4 as uuid } from "uuid"
+
+const eachDirector = directors.map((director) => 
+  <div key={uuid()}>{director.name}
+    <ul>{director.movies.map((movie) => 
+      <li key={uuid()}>{movie}</li>)}
+    </ul>
+  </div>)
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    {eachDirector}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { movies } from "../data";
+import { v4 as uuid } from "uuid"
+
+const eachMovie = movies.map((movie) => <div key={uuid()}>
+    {movie.title}, {movie.time}
+    <ul>
+      {movie.genres.map((genre) => <li key={uuid()}>{genre}</li>)}
+    </ul>
+  </div>)
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {eachMovie}
+  </div>;
 }
 
 export default Movies;
